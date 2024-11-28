6416a3d026705a6951e51c0837528185f8985e42
Karol Sekscinski <socer404@gmail.com>
2024-11-28 18:26:31 +0100

    for test (8 was too many)

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+try: [tf.config.experimental.set_memory_growth(gpu, True) for gpu in tf.config.experimental.list_physical_devices("GPU")]
+except: pass
 import pandas as pd
 from tqdm import tqdm
 
